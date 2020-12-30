7aae173a1fab9cb5472f81745158904bfbfebd8f
KuromameKota <haben.sie.das.licht.ausgemacht@gmail.com>
2020-12-30 18:55:03 +0900

    add debug log scroll view and music planel scene

---
+++ Assets/Scripts/Debugs/DebugLogScrollView.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Debugs
+{
+    public class DebugLogScrollView : MonoBehaviour
+    {
+        [SerializeField]
+        private Text debugText = null;
+
+        private void Awake()
+        {
+            Application.logMessageReceived += OnLogMessage;
+        }
+
+        private void OnDestroy()
+        {
+            Application.logMessageReceived -= OnLogMessage;
+        }
+
+        private void OnLogMessage(string logText, string stackTrace, LogType type)
+        {
+            if (string.IsNullOrEmpty(logText))
+            {
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(stackTrace))
+            {
+                switch (type)
+                {
+                    case LogType.Error:
+                    case LogType.Assert:
+                    case LogType.Exception:
+                        logText += System.Environment.NewLine + stackTrace;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            switch (type)
+            {
+                case LogType.Error:
+                case LogType.Assert:
+                case LogType.Exception:
+                    logText = string.Format("<color=red>{0}</color>", logText);
+                    break;
+                case LogType.Warning:
+                    logText = string.Format("<color=yellow>{0}</color>", logText);
+                    break;
+                default:
+                    break;
+            }
+
+            debugText.text += logText + System.Environment.NewLine;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/MusicPanelScene.cs
@@ -0,0 +1,64 @@
+﻿using UniRx;
+using System.Collections;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+using System;
+using System.IO;
+
+public class MusicPanelScene : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private string path = "/storage/emulated/0/Music/sample.mp3";
+
+    private void Start()
+    {
+        LoadAudio(path);
+    }
+
+    public void LoadAudio(string path)
+    {
+        if (Path.GetExtension(path) == ".m4a")
+        {
+            Debug.Log("Not supported audio format.");
+            return;
+        }
+
+        StartCoroutine(LoadToAudioClipAndPlay(path));
+    }
+
+    private IEnumerator LoadToAudioClipAndPlay(string path)
+    {
+        if (audioSource == null || string.IsNullOrEmpty(path))
+            yield break;
+
+        if (!File.Exists(path))
+        {
+            Debug.Log("File not found.");
+            yield break;
+        }
+
+        using (WWW www = new WWW("file://" + path))
+        {
+            while (!www.isDone)
+            {
+                yield return null;
+            }
+
+            AudioClip audioClip = www.GetAudioClip(false, true);
+            if (audioClip.loadState != AudioDataLoadState.Loaded)
+            {
+                Debug.Log("Failed to load AudioClip.");
+                yield break;
+            }
+
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            Debug.Log("Load success : " + path);
+        }
+    }
+
+
+}
