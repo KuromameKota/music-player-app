5ddc721170d1025a17b78c16d3d4da3d97c90a41
KuromameKota <haben.sie.das.licht.ausgemacht@gmail.com>
2021-01-12 22:01:46 +0900

    add UniRxExtensions #2

---
+++ Assets/Scripts/UniRxExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public static class UniRxExtensions
+    {
+        /// <summary>
+        /// lockTimeを指定した場合クリックした後にlockTimeの終了までクリックをできないようにする。
+        /// </summary>
+        /// <param name="button"></param>
+        /// <param name="lockTime"></param>
+        /// <returns></returns>
+        public static IObservable<Unit> OnClickAsObservable(this Button button, TimeSpan? lockTime = null)
+        {
+            var onClick = button.onClick.AsObservable();
+
+            if (lockTime.HasValue && lockTime.Value != TimeSpan.Zero)
+            {
+                onClick = onClick.ThrottleFirst(lockTime.Value);
+            }
+
+            return onClick;
+        }
+    }
+}
